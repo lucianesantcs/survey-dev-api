7de62aac032d5a1debe2c62ea14735173f1e47d0
Luciane Santos <lucianesantcs@gmail.com>
2024-01-21 20:27:32 -0300

    test: retirado describe duplicado

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -16,9 +16,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
-})
 
-describe('SignUp Controller', () => {
   test('should return 400 if no email is provided', () => {
     const signUpController = new SignUpController()
     const httpRequest = {
